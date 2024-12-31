65f0ef2d72630a7634569d37002c46c99d3c4be5
Akbarstore877 <akbarjakbar902@gmail.com>
2024-12-31 17:25:53 +0700

    AKBAR STORE ✅

--- setting/config.js
+++ setting/config.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-global.owner = ["6285742632270"];
-global.author = "RezaDevv";
+global.owner = ["6283841350073"];
+global.author = "Akbar stor";
 global.packname = "Push-Kontak";
 global.sessionName = "Bot-Session";
 global.versionbot = "1.2.0";
